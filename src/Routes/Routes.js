755d20dc0cdedef44f90f50db6b0a4e94fe356bb
RonitKumar09 <ronitkumar.robot@gmail.com>
2021-01-04 03:45:26 +0530

    Setup Basic Routes -components to be added later

---
+++ src/Routes/Routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+export default function Routes() {
+  <div>
+    <Switch>
+      <Route exact path="/Movielist" component={} />
+      <Route exact path="/FavouriteMovieList" component={} />
+      <Route exact path="/" component={} />
+    </Switch>
+  </div>;
+}
